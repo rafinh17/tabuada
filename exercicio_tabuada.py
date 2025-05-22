5c87e42aae59cca22b0006b1b13a5b9cea52840d
rafinh17 <fafa.macisilva15@gmail.com>
2025-05-22 17:40:37 -0300

    Add files via upload

---
+++ exercicio_tabuada.py
@@ -0,0 +1,7 @@
+numero = int(input("digite um numero:"))
+i = int(input("digite um numero:"))
+final= int(input('digite o numero que termina:'))
+
+while i <= final:
+    print(f"{i} x {numero} = {i * numero}")
+    i += 1
